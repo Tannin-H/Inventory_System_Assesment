ef75895b07738ebc585dfc245e7007ff6840bb6f
tanninheeley <tannin.heeley@gmail.com>
2021-05-06 15:08:12 +1200

    Add item button fully functional without input validation but newly added items can be restocked and sold

--- Inventory_System_Main.py
+++ Inventory_System_Main.py
@@ -36,7 +36,7 @@ class Inventory_GUI:
         save_btn.grid(column=3, row=0, sticky=E + W + N)
 
         self.canvas = Canvas(parent)
-        scroll_y = Scrollbar(parent, orient="vertical", command=self.canvas.yview)
+        self.scroll_y = Scrollbar(parent, orient="vertical", command=self.canvas.yview)
 
         self.frame = Frame(self.canvas, bg="white")
 
@@ -59,17 +59,14 @@ class Inventory_GUI:
                             variable=self.item_var))
             self.items_radbtns[item].grid(column=0, row=item + 1, padx=5, sticky=W + N)
 
-        for item in range(len(self.item_list)):
             self.price_lbls.append(
                 Label(self.frame, text="$" + str(self.item_list[item].price), bg="white", ))
             self.price_lbls[item].grid(column=1, row=item + 1, padx=5, sticky=W + N)
 
-        for item in range(len(self.item_list)):
             self.qty_lbls.append(
                 Label(self.frame, text=self.item_list[item].qty, bg="white", ))
             self.qty_lbls[item].grid(column=2, row=item + 1, padx=5, sticky=W + N)
 
-        for item in range(len(self.item_list)):
             self.seller_lbls.append(
                 Label(self.frame, text=self.item_list[item].vendor, bg="white", anchor="w"))
             self.seller_lbls[item].grid(column=3, row=item + 1, padx=5, sticky=W + N)
@@ -80,10 +77,10 @@ class Inventory_GUI:
         self.canvas.update_idletasks()
 
         self.canvas.configure(scrollregion=self.canvas.bbox('all'),
-                              yscrollcommand=scroll_y.set, width=400)
+                              yscrollcommand=self.scroll_y.set, width=400)
 
         self.canvas.grid(column=0, row=2, columnspan=4, sticky=E)
-        scroll_y.grid(column=4, row=2, sticky=N + S)
+        self.scroll_y.grid(column=4, row=2, sticky=N + S)
 
     def sell_item(self):
         items_sold = simpledialog.askinteger(title="Sell Item Entry", prompt="Enter number of items to be sold")
@@ -104,38 +101,76 @@ class Inventory_GUI:
                                  "Input not valid please enter a positive value")
             items_restocked = simpledialog.askinteger(title="Restock Item Entry", prompt="Enter number of items to be "
                                                                                          "Restocked")
+        print(self.item_var.get())
         restocked_qty = self.item_list[self.item_var.get()].qty + items_restocked
         self.item_list[self.item_var.get()].qty = restocked_qty
         self.qty_lbls[self.item_var.get()].configure(text=restocked_qty)
 
     def add_item(self):
-        splash_screen = Toplevel(root)
+        self.splash_screen = Toplevel(root)
         lbl_names = ["Name:", "Price:", "Quantity:", "Vendor:"]
         lbl_list = []
 
-        descrp_lbl = Label(splash_screen, text="Please Enter Product Details")
+        descrp_lbl = Label(self.splash_screen, text="Please Enter Product Details")
         descrp_lbl.grid(column=0, row=0, columnspan=2, sticky=E + W)
 
         for name in range(len(lbl_names)):
-            lbl_list.append(Label(splash_screen, text=lbl_names[name]))
+            lbl_list.append(Label(self.splash_screen, text=lbl_names[name]))
             lbl_list[name].grid(column=0, row=name + 1)
 
-        name_var = StringVar()
-        price_var = IntVar()
-        qty_var = IntVar()
-        vendor_var = StringVar()
+        self.name_var = StringVar()
+        self.price_var = StringVar()
+        self.qty_var = StringVar()
+        self.vendor_var = StringVar()
 
-        name_entry = Entry(splash_screen, textvariable=name_var)
+        name_entry = Entry(self.splash_screen, textvariable=self.name_var)
         name_entry.grid(column=1, row=1)
 
-        price_entry = Entry(splash_screen, textvariable=price_var)
+        price_entry = Entry(self.splash_screen, textvariable=self.price_var)
         price_entry.grid(column=1, row=2)
 
-        qty_entry = Entry(splash_screen, textvariable=qty_var)
+        qty_entry = Entry(self.splash_screen, textvariable=self.qty_var)
         qty_entry.grid(column=1, row=3)
 
-        vendor_entry = Entry(splash_screen, textvariable=vendor_var)
+        vendor_entry = Entry(self.splash_screen, textvariable=self.vendor_var)
         vendor_entry.grid(column=1, row=4)
+
+        submit_btn = Button(self.splash_screen, text="Submit Product", command=self.destroy_splashscreen)
+        submit_btn.grid(column=0, row=5, columnspan=2, sticky=E + W, pady=10)
+
+    def destroy_splashscreen(self):
+        self.item_list.append(
+            Item(self.name_var.get(), int(self.price_var.get()), int(self.qty_var.get()), self.vendor_var.get()))
+        self.update_list()
+        self.splash_screen.destroy()
+        self.splash_screen.update()
+
+    def update_list(self):
+        self.items_radbtns.append(
+            Radiobutton(self.frame, text=self.item_list[-1].name, bg="white", value=len(self.item_list) - 1,
+                        variable=self.item_var))
+        self.items_radbtns[-1].grid(column=0, row=len(self.item_list) + 1, padx=5, sticky=W + N)
+
+        self.price_lbls.append(
+            Label(self.frame, text="$" + str(self.item_list[-1].price), bg="white", ))
+        self.price_lbls[-1].grid(column=1, row=len(self.item_list) + 1, padx=5, sticky=W + N)
+
+        self.qty_lbls.append(
+            Label(self.frame, text=self.item_list[-1].qty, bg="white", ))
+        self.qty_lbls[-1].grid(column=2, row=len(self.item_list) + 1, padx=5, sticky=W + N)
+
+        self.seller_lbls.append(
+            Label(self.frame, text=self.item_list[-1].vendor, bg="white", anchor="w"))
+        self.seller_lbls[-1].grid(column=3, row=len(self.item_list) + 1, padx=5, sticky=W + N)
+
+        self.canvas.update_idletasks()
+
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'),
+                              yscrollcommand=self.scroll_y.set, width=400)
+
+        print(len(self.item_list))
+
+        # This version displays the correct values and works but breaks when you try and sell a product
 
     def save_inventory(self):
         pass
